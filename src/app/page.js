ac5bcc371c212f8e32f33d755420c65f51d1e149
Amisha Choudhary <103454929+Amishachoudhary22@users.noreply.github.com>
2025-01-29 12:26:36 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -18,10 +18,10 @@ export default function Home() {
           />
           <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
             <p>
-              Welcome to Tasty Foods, where we bring your favorite meals to your doorstep with just a few taps! Our mission is to connect you with local restaurants, ensuring fresh, delicious, and diverse cuisine is always within reach. Whether you're craving a quick snack, a hearty meal, or an indulgent dessert, we've got you covered.
+              Welcome to Tasty Foods, where we bring your favorite meals to your doorstep with just a few taps! Our mission is to connect you with local restaurants, ensuring fresh, delicious, and diverse cuisine is always within reach. Whether you&apos;re craving a quick snack, a hearty meal, or an indulgent dessert, we&apos;ve got you covered.
             </p>
             <p>
-              Our platform is designed for speed, simplicity, and satisfaction, delivering not just food but also a seamless experience. With a focus on quality and reliability, we're here to make your mealtime moments more enjoyable. Sit back, relax, and let us do the rest!
+              Our platform is designed for speed, simplicity, and satisfaction, delivering not just food but also a seamless experience. With a focus on quality and reliability, we&apos;re here to make your mealtime moments more enjoyable. Sit back, relax, and let us do the rest!
             </p>
           </div>
         </section>
